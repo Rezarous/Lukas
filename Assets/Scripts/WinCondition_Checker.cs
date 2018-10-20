c0cd648e70b7ecf4ce0cc250e1bc260a206cf4db
Reza.Rustaee <reza.rustaee@gmail.com>
2018-10-20 18:53:17 +0200

    Project basics

---
+++ Assets/Scripts/WinCondition_Checker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition_Checker : MonoBehaviour {
+
+    public GameObject[] planets;
+    public GameObject[] rings;
+
+    int numberofPlaents;
+
+	// Use this for initialization
+	void Start () {
+
+        numberofPlaents = Behaviour_Manager.numberOfPlanets;
+        planets = new GameObject[numberofPlaents];
+        rings = new GameObject[numberofPlaents];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if(rings[0].transform.position == planets[0].transform.position){
+        //    print("yes");
+        //}
+
+	}
+}
